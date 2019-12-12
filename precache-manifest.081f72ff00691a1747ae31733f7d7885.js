fe12daa6a637224d53af361c5f23f5fa0475649b
Juri Em <juriem1974@gmail.com>
2019-12-12 13:14:46 +0200

    Updates

---
+++ precache-manifest.081f72ff00691a1747ae31733f7d7885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ce2ac8ac274719f991082361ef13ce",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "30704214083663e78e25",
+    "url": "/mosters-rolodex/static/css/main.d3b09365.chunk.css"
+  },
+  {
+    "revision": "0193fd501bbadc1438a6",
+    "url": "/mosters-rolodex/static/js/2.c9114c39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mosters-rolodex/static/js/2.c9114c39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30704214083663e78e25",
+    "url": "/mosters-rolodex/static/js/main.ac5f311c.chunk.js"
+  },
+  {
+    "revision": "26aec216abccfb73fa57",
+    "url": "/mosters-rolodex/static/js/runtime-main.5dcf83dc.js"
+  }
+]);
